c097ed52c485e642f3e3c580e8443439606b13ed
Toni <antonio.kamber@jobiqo.com>
2022-10-07 17:26:41 +0200

    Add graphql

--- plenkovic-apartments/pages/apartments/index.tsx
+++ plenkovic-apartments/pages/apartments/index.tsx
@@ -1,10 +1,5 @@
-import * as contentful from "contentful";
-
-const client = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE,
-  accessToken: process.env.CONTENTFUL_TOKEN,
-//   host: "preview.contentful.com",
-});
+import { IApartmentsCollection } from "@m/apartment";
+import { getApartments } from "@u/contentful";
 
 interface ApartmentsProps {
   title: string;
@@ -14,11 +9,11 @@ export default function Apartments({ title }: ApartmentsProps) {
 }
 
 export async function getStaticProps() {
-  const apartment = await client.getEntry("6IyeOn3nL84nOkMU2FyUGZ")
+  const data: IApartmentsCollection = await getApartments();
 
   return {
     props: {
-      title: apartment.fields.title,
+      title: data.apartmentCollection.items[0].title,
     },
   };
 }
